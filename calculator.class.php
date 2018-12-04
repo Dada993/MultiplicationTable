3af35f5f48de1c70e5be14d88319c43d15e2c7f4
Dada993 <45605717+Dada993@users.noreply.github.com>
2018-12-04 20:10:05 +0100

    Update calculator.class.php

--- calculator.class.php
+++ calculator.class.php
@@ -3,18 +3,19 @@
 class calculator{
 	
 	//varijable
-    private $red;
+    	private $red;
 	private $kol;
 	
 	//konstruktor
 	function __construct($red1, $kol1){
-		$this->red = $red1;
+	$this->red = $red1;
         $this->kol = $kol1;
     }
  
 	//metod za računanje i vraćanje vrijednosti
-    function multy(){
+    	function multy(){
 		return $this->red * $this->kol; 
 	}
 }
+
 ?>
